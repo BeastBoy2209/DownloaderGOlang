cb02bfbfc91be4110049887666e410231b121924
BeastBoy <58637567+BeastBoy2209@users.noreply.github.com>
2026-08-07 12:03:07 +0500

    ++graceful shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	"errors"
+
 	_ "github.com/lib/pq"
 
 	"downloader/internal/config"
@@ -22,22 +29,47 @@ func main() {
 	if err != nil {
 		log.Fatalf("sql can't open db %v", err)
 	}
-	defer db.Close()
+	
+	defer db.Close() 
+	
 	if err := db.Ping(); err != nil {
 		log.Fatalf("database cant start %v", err)
 	}
-	log.Println("OK")
+	log.Println("DB OK")
 
-	repo := repository.NewPostgresRepo(db)       
+	repo := repository.NewPostgresRepo(db)
 	service := usecase.NewDownloadService(repo)
-	handler := transport.NewHandler(service)      
+	handler := transport.NewHandler(service)
 
 	mux := handler.InitRoutes()
 
 	addr := fmt.Sprintf(":%d", cfg.Server.Port)
 	log.Printf("PORT: %s", addr)
 
-	if err := http.ListenAndServe(addr, mux); err != nil {
-		log.Fatalf("Error %v", err)
+	server := &http.Server{
+		Addr:    addr,
+		Handler: mux, 
 	}
+
+	go func() {
+		log.Printf("Server started on port %d", cfg.Server.Port)
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("server error %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+	log.Println("Starting shutdown...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server was stopped cause error or timeout: %v", err)
+	}
+
+	log.Println("Server was successfully shut down")
 }
